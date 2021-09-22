e2406904edd83a3740ff14eda69691c404a2adb0
Ricardo Salazar <no-replay>
2021-09-21 21:49:36 -0500

    add navbar and css style name logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
-    <h2>React-Portfolio</h2>
+    <Navbar />
   );
 }
 
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../logo.png"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <a className="navbar-brand" href="#"><img src={logo} alt="logo..."/></a>
+      <button ClassName="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span ClassName="navbar-toggler-icon"></span>
+      </button>
+
+      <div clasName="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Abot me <span class="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact Me</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#"></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
