a1e36cf92277bc69a7787c83453d688964d502c6
Ricardo Salazar <no-replay>
2021-09-23 19:02:49 -0500

    add header and style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
+    <>
     <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ my-app/src/components/header.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>
+                    Full Stack Web Developer
+                </h1><br></br>
+                <p>Studied at University of Texas at San Antonio, full stack web developer Bootcamp, learned the languages used in web developing,
+                    passionated about new technologies and how everything works behind peoples eyes. 
+                </p>
+
+            </div>
+        </div>
+    )
+}
+export default Header;
